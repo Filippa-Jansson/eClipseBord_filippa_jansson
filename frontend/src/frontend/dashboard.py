e5795f93b97241a57342cf2ab8c91dc2c50144cc
filippa.j <filippa762@gmail.com>
2026-09-03 17:52:05 +0200

    frontend and backend working

--- frontend/src/frontend/dashboard.py
+++ frontend/src/frontend/dashboard.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import httpx
 import pandas as pd
+import os
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
 
 def main():
     st.markdown("# eClipseBord dashboard")
