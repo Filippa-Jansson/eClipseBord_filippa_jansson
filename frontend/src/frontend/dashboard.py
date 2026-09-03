8c8c9e1414530686d373ac0107286d5e8b9ca280
filippa.j <filippa762@gmail.com>
2026-09-03 15:40:12 +0200

    streamlit try out

--- frontend/src/frontend/dashboard.py
+++ frontend/src/frontend/dashboard.py
@@ -1,10 +1,23 @@
 import streamlit as st
 import httpx
+import pandas as pd
 
 BASE_URL = "http://127.0.0.1:8000"
 
 def main():
     st.markdown("# Dashboard")
-    
+
+    lunar_response = httpx.get(f"{BASE_URL}/lunar/data")
+    solar_response = httpx.get(f"{BASE_URL}/solar/data")
+
+    lunar_df = pd.DataFrame(lunar_response.json())
+    solar_df = pd.DataFrame(solar_response.json())
+
+    st.markdown("## Lunar data")
+    st.dataframe(lunar_df)
+
+    st.markdown("## Solar data")
+    st.dataframe(solar_df)
+
 if __name__ == "__main__":
     main()
